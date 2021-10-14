5f54301efd98f21cd033f1cab027ee6c633c46e9
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-14 20:37:38 -0300

    completo cambiofinal

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,16 @@ let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("usuario");
 let dato2 = document.getElementById("clave");
 btnEnv.addEventListener("click", () => {
-let usuario: string = ""
+
+  let usuario: string = ""
 let clave: string = ""
 
 let usuarioingresado: string = usuario.value
 let claveingresada: string = clave.value 
-if(usuario == usuarioingresado && clave == claveingresada)}{
- 
-  console.log:("bienvenido");
-  
+if(usuario == usuarioingresado && clave == claveingresada){
+  console.log("ingreso de usuario")
 }else{
-
-  console.log:("incorrecto")
+  console.log("usuario incorrecto")
+}
 
 }
