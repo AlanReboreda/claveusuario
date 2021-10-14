b07f41c084406fd5386560ce2a855fe3de9de1e3
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-14 20:11:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("usuario");
+let dato2 = document.getElementById("clave");
+btnEnv.addEventListener("click", () => {
+let usuario: string = ""
+let clave: string = ""
+
+let usuarioingresado: string = usuario.value
+let claveingresada: string = clave.value 
+if(usuario == usuarioingresado && clave == claveingresada)}{
+ 
+  console.log:("bienvenido");
+  
+}else{
+
+  console.log:("incorrecto")
+
+}
